7195c91231aa1090363a409fe70a3e91e6ff0113
Wessh <wesshbr@gmail.com>
2022-12-13 17:25:12 -0300

    ajustado o controller para receber os dados do Post e enviar para o db. adicionado as dependencias de projeto : class-validator e class-transformer. Criado uma classe para receber e validar os dados recebidos no POST

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { randomUUID } from 'node:crypto';
+import { CreateNotificationBody } from './create-notification-body';
 
 @Controller('notifications')
 export class AppController {
@@ -11,13 +12,14 @@ export class AppController {
     return this.prisma.notification.findMany();
   }
   @Post()
-  async create() {
+  async create(@Body() body: CreateNotificationBody) {
+    const { recipientId, content, category } = body;
     await this.prisma.notification.create({
       data: {
         id: randomUUID(),
-        content: '200 ok',
-        category: 'Server',
-        recipientId: randomUUID(),
+        content,
+        category,
+        recipientId,
       },
     });
   }
